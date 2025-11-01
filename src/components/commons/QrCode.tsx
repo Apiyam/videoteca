000fd179403084b95b3e5178b47db9963ebbb717
fernandoGABS <gsfo115485@gmail.com>
2025-11-01 17:29:06 -0600

    updated

--- src/components/commons/QrCode.tsx
+++ src/components/commons/QrCode.tsx
@@ -1,4 +1,3 @@
-import { QRCodeSVG } from "qrcode.react";
 import { useAppContext } from 'AppContext';
 import { Box } from "@mui/joy";
     
@@ -6,24 +5,7 @@ export default function QrCode() {
     const { companyData } = useAppContext();
     return (
         <Box alignItems="center" justifyContent="center" display="flex" height="100%">
-            <QRCodeSVG
-                value={companyData.website}
-                title={companyData.name}
-                size={200}
-                bgColor={"#ffffff"}
-                fgColor={companyData.config.strongColor}
-                level={"L"}
-                imageSettings={{
-                    src: companyData.logo,
-                    x: undefined,
-                    y: undefined,
-                    height: 80,
-                    width: 80,
-                    opacity: 0.9,
-                    excavate: true,
-                }}
-                />
-                <br />
+            
         </Box>
     )
 }
